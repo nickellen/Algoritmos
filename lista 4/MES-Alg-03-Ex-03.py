58a421162bb4d1b4c6b3ba0e14c7ab1dfeb50e48
ellen <98707165+nickellen@users.noreply.github.com>
2022-06-23 23:26:45 -0300

    Delete lista 4 directory

--- lista 4/MES-Alg-03-Ex-03.py
+++
@@ -1,7 +0,0 @@
-# Vogal ou consoante
-letra = str(input('Insira uma letra do alfabeto:'))
-
-if (letra== 'a') or (letra== 'e') or (letra== 'i') or (letra== 'o') or (letra== 'u'):
-    print('Letra vogal')
-else:
-    print('Letra consoante')
