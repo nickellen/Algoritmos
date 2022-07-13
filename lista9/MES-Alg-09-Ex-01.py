af15e8f061bbae217692f0b01ecfeb178b667e7f
monique <98707165+nickellen@users.noreply.github.com>
2022-07-13 17:34:40 -0300

    Update MES-Alg-09-Ex-01.py

--- lista9/MES-Alg-09-Ex-01.py
+++ lista9/MES-Alg-09-Ex-01.py
@@ -1,1 +1,7 @@
+# Passagem de argumentos para o programa em linha de comando
+import sys
 
+if __name__=="__main__":
+    print(f"Número de argumentos: {len(sys.argv)}")
+    for i, arg in enumerate(sys.argv):
+        print(f"Argumento {i}: {arg}")
